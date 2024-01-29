879cfa12ea1ba222eed1893dbebae7ec36ad2ce8
selamawityilma <selamawit.woldeyohannes@miu.edu>
2024-01-28 19:19:59 -0800

    [portfolio] blog added

--- src/app/apps/blogs/blogs.component.ts
+++ src/app/apps/blogs/blogs.component.ts
@@ -7,17 +7,38 @@ import { Component, OnInit } from '@angular/core';
 export class BlogsComponent {
 
   id: any;
-  blogDetail = {
-    id: 1,
-    image: 'assets/images/blog/101.jpg',
-    heading:
-      'Magna nostrud incididunt cupidatat cillum incididunt et nisi in ut minim reprehenderit.',
-    subHeading: 'Labore irure irure laborum quis tempor aliqua. Dude',
-    blogAuthor: 'Selamawit W',
-    blogDate: 'Feb 19, 2020',
-    blogDetail:
-      'Est ipsum sint officia quis nulla nisi cupidatat aliquip nisi laboris eiusmod eiusmod aliquip do. Commodo elit excepteur occaecat irure Lorem sit nulla nulla sint duis incididunt. Nostrud ut do mollit et amet velit aute excepteur et culpa culpa velit. Cillum veniam officia anim cupidatat.',
-  };
-
-
+  blogDetails = [
+    {
+      id: 1,
+      image: 'assets/images/innerpage/design.png',
+      heading: 'Hosting a Static Website (Angular App) on AWS with Route 53, S3, and AWS Certificate Manager',
+      blogAuthor: 'Selamawit W',
+      blogDate: 'Jan 28, 2024',
+      blogDetail: `
+      This blog post provides an overview of hosting a static website on AWS using Route 53, S3,
+      and AWS Certificate Manager, and discusses the benefits of using Amazon CloudFront as a
+      content delivery network. It covers essential steps for setting up DNS management, hosting
+      static content on S3, configuring HTTPS encryption, and compares hosting with and without CloudFront.
+      Whether you're a beginner looking to host your first static website or a seasoned developer exploring
+      AWS's hosting capabilities, this guide serves as a comprehensive resource for deploying static web
+      applications on the AWS cloud platform.`,
+      blogLink: 'https://medium.com/@selamawitwoldey/hosting-a-static-website-angular-app-on-aws-with-route-53-s3-and-aws-certificate-manager-bc92bdfaea42'
+    },
+    {
+      id: 2,
+      image: 'assets/images/innerpage/dotnetsnowflake.png',
+      heading: 'Leveraging Snowflake in .NET Micro services',
+      blogAuthor: 'Selamawit W',
+      blogDate: 'Jan 28, 2024',
+      blogDetail: `
+      This blog post provides an overview of how to use Snowflake in .NET microservices,
+      discussing its benefits, challenges, and considerations for integration.
+      It covers essential aspects such as scalability, performance, flexibility, and management,
+      offering insights for developers and organizations looking to leverage Snowflake for building scalable and
+      data-driven microservices-based applications. Whether you're a .NET developer exploring new data analytics solutions or
+      an organization seeking to harness the power of Snowflake, this guide serves as a comprehensive resource for understanding
+      the integration of Snowflake with .NET microservices.`,
+      blogLink: 'https://medium.com/@selamawitwoldey/leveraging-snowflake-in-net-micro-services-b49d2bde5dd0'
+    }
+  ];
 }
